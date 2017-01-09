91768bc45397f3ed566efd52ee06acbd8e823e50
Ian Later <Ian@VictorioussMBP4.lan>
2017-01-09 01:51:47 -0800

    bigger

--- processing/sketch.js
+++ processing/sketch.js
@@ -6,7 +6,7 @@ var ripple = function(p, r = 20, c = 20, s = 60){
     var toggle = false;
 
     p.setup = function() {
-      canvas = p.createCanvas(600,  600);
+      canvas = p.createCanvas(1618,1000);
       p.frameRate(20);
       rows = p.ceil(p.height/scl)+1;
       columns = p.ceil(p.width/scl)+1;
@@ -37,14 +37,14 @@ var ripple = function(p, r = 20, c = 20, s = 60){
     else
       drawTriangleStrip();
   };
-  
+
   p.touchStarted = function(){
     mouseClicked();
   }
   p.mouseClicked = function() {
     if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY < p.height) {
         toggle = !toggle;
-    } 
+    }
   }
 
   function drawCircles() {
@@ -53,7 +53,7 @@ var ripple = function(p, r = 20, c = 20, s = 60){
         var z = ar[y][x].z+1;
         p.fill(100*z-127, 100*z, 255 *z-127);
         // text(nf(sin(ar[y][x].z), 1, 2), ar[y][x].x, ar[y][x].y);
-        var r = scl * (z+.2) * 1.2;
+        var r = scl * (z+.18) * 1.5;
         p.ellipse(ar[y][x].x, ar[y][x].y, r, r);
       }
     }
@@ -77,4 +77,3 @@ var ripple = function(p, r = 20, c = 20, s = 60){
   };
 };
 var l = new p5(ripple, 'leftCanvas');
-var r = new p5(ripple, 'rightCanvas');
