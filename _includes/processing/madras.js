d572b7b43eb2945cb3ba7eb9caa80491e4762e65
Ian Later <Ian@VictorioussMBP4.lan>
2017-02-07 00:08:41 -0800

    gridmodulo

--- _includes/processing/madras.js
+++ _includes/processing/madras.js
@@ -1,19 +1,19 @@
 var madras = function( p ) {
   var mapToBranches = [];
   var branches = [];
-  var maxBranches = 30;
+  var maxBranches = 20;
   var degree = 3;
   var line = false;
   var toggle = false;
   var count = 0;
   var maxAge = 10;
-  var gridModulator = 0;
+  var gridModulator = 3;
   var goodcolor, me, brushSlider, fadeSlider;
 
   p.setup = function() {
     brushSlider = p.createSlider(0,15, 4, .33);
     fadeSlider = p.createSlider(90,255, 100);
-    p.createButton("Grid").mousePressed(function(){gridModulator++;});
+    p.createButton("Grid").mousePressed(function(){gridModulator = ++gridModulator > 2 ? 0 : gridModulator;});
     p.createButton("Pause").mousePressed(pause);
     p.createButton("Reset").mousePressed(refresh);
     goodcolor = [p.color("#000000"), p.color("#000000"), p.color("#000000"), p.color("#6b6556"), p.color("#a09c84"), p.color("#908b7c"), p.color("#79746e"), p.color("#755d35"), p.color("#937343"), p.color("#9c6b4b"), p.color("#ab8259"), p.color("#aa8a61"), p.color("#578375"), p.color("#f0f6f2"), p.color("#d0e0e5"), p.color("#d7e5ec"), p.color("#d3dfea"), p.color("#c2d7e7"), p.color("#a5c6e3"), p.color("#a6cbe6"), p.color("#adcbe5"), p.color("#77839d"), p.color("#d9d9b9"), p.color("#a9a978"), p.color("#727b5b"), p.color("#6b7c4b"), p.color("#546d3e"), p.color("#47472e"), p.color("#727b52"), p.color("#898a6a"), p.color("#919272"), p.color("#AC623b"), p.color("#cb6a33"), p.color("#9d5c30"), p.color("#843f2b"), p.color("#652c2a"), p.color("#7e372b"), p.color("#403229"), p.color("#47392b"), p.color("#3d2626"), p.color("#362c26"), p.color("#57392c"), p.color("#998a72"), p.color("#864d36"), p.color("#544732") ];
@@ -62,9 +62,10 @@ var madras = function( p ) {
     this.equation = function(x,y){
       return p.createVector(x,y).add(this.velocity);
     };
-    if (gridModulator % 3 == 0){
+    console.log(gridModulator);
+    if (gridModulator == 2){
       random = p.random() > .5 ? p.noise(p.TWO_PI) : p.randomGaussian(p.TWO_PI);
-    } else if (gridModulator % 2 == 0){
+    } else if (gridModulator == 1){
       random = p.randomGaussian(p.TWO_PI);
     } else {
       random = p.noise(p.HALF_PI);
